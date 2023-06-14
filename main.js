3657546b86d81a670a4b2d55958c74ccb4f3f454
Nassim's Software <128189209+JSLegendDev@users.noreply.github.com>
2023-06-13 21:47:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 import kaboom from "https://unpkg.com/kaboom@3000.0.1/dist/kaboom.mjs"
 
-kaboom()
+kaboom({
+    width: 1280,
+    height: 720,
+    letterbox: true
+})
+
+loadSpriteAtlas('./assets/Player.png' ,{
+    'idle-sprites': {
+        x: 0,
+        y: 16,
+        width: 192,
+        height: 16,
+        sliceX: 4,
+        sliceY: 1,
+        anims: {
+            'idle': {
+                from: 0,
+                to: 3,
+                loop: true,
+                speed: 8
+            }
+        }
+    }
+})
+
+add([sprite('idle-sprites', {anim: 'idle'}), pos(center()), scale(4), area()])
